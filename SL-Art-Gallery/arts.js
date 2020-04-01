8a4ed29b9532534ffff22a8b32ba535811680a98
Abdul Moqueet <36290215+Abdul-Moqueet@users.noreply.github.com>
2020-04-01 19:17:44 +0530

    Update arts.js

--- SL-Art-Gallery/arts.js
+++ SL-Art-Gallery/arts.js
@@ -1,18 +1,82 @@
 var arts = [
     {
-        name:"Sick",
-        link:"https://github.com/Abdul-Moqueet/solo-files/raw/master/SL-Art-Gallery/1.jpg"
+        name:"Sick L̲̅i̲̅n̲̅e̲̅ B̶r̶o̶🌡️",
+        link:"1.jpg"
     },
     {
-        name:"Sick",
-        link:"https://github.com/Abdul-Moqueet/solo-files/raw/master/SL-Art-Gallery/2.jpg"
+        name:"Sick L̲̅i̲̅n̲̅e̲̅ B̶r̶o̶🌡️",
+        link:"2.jpg"
     },
     {
         name:"Abhinaya",
-        link:"https://github.com/Abdul-Moqueet/solo-files/raw/master/SL-Art-Gallery/3.jpg"
+        link:"3.jpg"
     },
     {
-        name:"Sparshika",
-        link:"https://github.com/Abdul-Moqueet/solo-files/raw/master/SL-Art-Gallery/4.jpg"
+        name:"ѕραяѕнιкα 🎀",
+        link:"4.jpg"
+    },
+    
+    {
+        name:"ѕραяѕнιкα 🎀",
+        link:"5.jpg"
+    },
+    
+    {
+        name:"Mushkan Bhatia",
+        link:"6.jpg"
+    },
+    {
+        name:"SR",
+        link:"7.jpg"
+    },
+    {
+        name:"Bad_Bit",
+        link:"8.jpg"
+    },
+    {
+        name:"Mushkan Bhatia",
+        link:"9.jpg"
+    },
+    {
+        name:"Al  Faiaj  Anon",
+        link:"10.jpg"
+    },
+    {
+        name:"°°MēDh@°°",
+        link:"11.jpg"
+    },
+    {
+        name:"Muhammad Abdurahman",
+        link:"12.jpg"
+    },
+    {
+        name:"ᏃᎪᏟᏦ✰",
+        link:"13.jpg"
+    },
+    {
+        name:"ManishA Sahu 😉",
+        link:"14.jpg"
     }
+    ,{
+        name:"🐉Tanay 🐉",
+        link:"15.jpg"
+    },
+    {
+        name:"Arb Rahim Badsa",
+        link:"16.jpg"
+    },
+    {
+        name:"RKK",
+        link:"17.jpg"
+    },
+    {
+        name:"Amine Laaboudi",
+        link:"18.jpg"
+    },
+    {
+        name:"Muhammad Abdurahman",
+        link:"19.jpg"
+    }    
 ];
+
+var baseUrl = "https://abdul-moqueet.github.io/solo-files/SL-Art-Gallery/";
