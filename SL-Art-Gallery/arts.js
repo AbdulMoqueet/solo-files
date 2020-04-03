a9eccb3b1585cbdf66aa294090c2161fed90091b
Abdul Moqueet <36290215+Abdul-Moqueet@users.noreply.github.com>
2020-04-03 18:41:51 +0530

    Update arts.js

--- SL-Art-Gallery/arts.js
+++ SL-Art-Gallery/arts.js
@@ -23,123 +23,168 @@ var _0x1ce1 = function (_0x14c6bb, _0x2c6d8d) {
 var footer = document[_0x1ce1('0x3')](_0x1ce1('0x0'));
 footer[_0x1ce1('0x4')] = _0x1ce1('0x5');
 document[_0x1ce1('0x1')][_0x1ce1('0x2')](footer);
+    
+    
+    const loader = document.querySelector(".loader");
+    var img = document.createElement("img");
+    img.src = "https://abdul-moqueet.github.io/solo-files/SL-Art-Gallery/the_blood_coder.jpg";
+    loader.appendChild(img);
+    
+    var text = document.createElement("p");
+    text.innerHTML = "Loading...";
+    loader.appendChild(text);
+    
+    
 } );
 
 var arts = [
     {
         name:"Sick L̲̅i̲̅n̲̅e̲̅ B̶r̶o̶🌡️",
-        link:"1.jpg"
+        link:"1.jpg",
+        post:"277977"
     },
     {
         name:"ѕραяѕнιкα 🎀",
-        link:"4.jpg"
+        link:"4.jpg",
+        post:"280362"
     },
     {
         name:"Abhinaya",
-        link:"3.jpg"
+        link:"3.jpg",
+        post:"280055"
     },
     {
         name:"Sick L̲̅i̲̅n̲̅e̲̅ B̶r̶o̶🌡️",
-        link:"2.jpg"
+        link:"2.jpg",
+        post:"278792"
     },
-    
     {
         name:"ѕραяѕнιкα 🎀",
-        link:"5.jpg"
+        link:"5.jpg",
+        post:"279964"
     },
-    
     {
         name:"Mushkan Bhatia",
-        link:"6.jpg"
+        link:"6.jpg",
+        post:"279150"
     },
     {
         name:"SR",
-        link:"7.jpg"
+        link:"7.jpg",
+        post:"278114"
     },
     {
         name:"Bad_Bit",
-        link:"8.jpg"
+        link:"8.jpg",
+        post:"277980"
     },
     {
         name:"Mushkan Bhatia",
-        link:"9.jpg"
+        link:"9.jpg",
+        post:"279149"
     },
     {
         name:"Al  Faiaj  Anon",
-        link:"10.jpg"
+        link:"10.jpg",
+        post:"278240"
     },
     {
         name:"°°MēDh@°°",
-        link:"11.jpg"
+        link:"11.jpg",
+        post:"279083"
     },
     {
         name:"Muhammad Abdurahman",
-        link:"12.jpg"
+        link:"12.jpg",
+        post:"11956"
     },
     {
         name:"ᏃᎪᏟᏦ✰",
-        link:"13.jpg"
+        link:"13.jpg",
+        post:"279711"
     },
     {
         name:"ManishA Sahu 😉",
-        link:"14.jpg"
+        link:"14.jpg",
+        post:"280251"
     }
     ,{
         name:"🐉Tanay 🐉",
-        link:"15.jpg"
+        link:"15.jpg",
+        post:"278960"
     },
     {
         name:"Arb Rahim Badsa",
-        link:"16.jpg"
+        link:"16.jpg",
+        post:"279151"
     },
     {
         name:"N.N.I",
-        link:"23.jpg"
+        link:"23.jpg",
+        post:"132210"
     },
     {
         name:"RKK",
-        link:"17.jpg"
+        link:"17.jpg",
+        post:"279190"
     },
     {
         name:"Amine Laaboudi",
-        link:"18.jpg"
+        link:"18.jpg",
+        post:"278448"
     },
     {
         name:"Muhammad Abdurahman",
-        link:"19.jpg"
+        link:"19.jpg",
+        post:"280205"
     },
     {
         name:"Chinonso",
-        link:"20.jpg"
+        link:"20.jpg",
+        post:"281139"
     },
     {
         name:"Raj Sadaye",
-        link:"21.jpg"
+        link:"21.jpg",
+        post:"280306"
     },
     {
         name:"ꌗꋬ✴",
-        link:"22.jpg"
+        link:"22.jpg",
+        post:"279976"
     },
     {
         name:"Chicx",
-        link:"24.jpg"
+        link:"24.jpg",
+        post:"281765"
     },
     {
         name:"Shreyansh",
-        link:"25.jpg"
+        link:"25.jpg",
+        post:"281715"
     },
     {
         name:"Kavi Rash",
-        link:"26.jpg"
+        link:"26.jpg",
+        post:"279814"
     },
     {
         name:"Kratika Soni",
-        link:"27.jpg"
+        link:"27.jpg",
+        post:"282029"
+    },
+    {
+        name:"ManishA Sahu 😉",
+        link:"28.jpg",
+        post:"282635"
     },
     {
         name:"Sick L̲̅i̲̅n̲̅e̲̅ B̶r̶o̶🌡️",
-        link:"end.jpg"
+        link:"end.jpg",
+        post:"281811"
     }
 ];
 
-var baseUrl = "https://abdul-moqueet.github.io/solo-files/SL-Art-Gallery/";
+const baseUrl = "https://abdul-moqueet.github.io/solo-files/SL-Art-Gallery/";
+const baseLink="https://www.sololearn.com/post/";
+const suffix="?ref=app";
